2e58ac6f75448ae173fdc6b3b31b8f504e7caab0
rastarad <63683606+rastarad@users.noreply.github.com>
2020-11-21 13:08:11 +0100

    Remove  unnecessary file

--- web-services/index.js
+++ web-services/index.js
@@ -1,9 +1,22 @@
+const yup = require("yup");
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
-var bodyParser = require("body-parser");
+let schema = yup.object().shape({
+  name: -yup
+    .string()
+    .trim()
+    .max(10)
+    .matches(/^[a-zA-Z]+$/)
+    .required(),
+});
+
 app.use(helmet());
 
 app.get("/", (req, res) => {
@@ -34,4 +47,33 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-// node ./app.js - start serwera w terminalu
+//zad.1
+app.get("/info", (req, res) => {
+  res.status(200);
+  res.json({ author: "1234123" });
+});
+
+//zad.2
+app.get("/hello/:name", (req, res) => {
+  schema.isValid(req.params).then(function (valid) {
+    if (valid) res.send(`Hello ${req.params.name}!`);
+    else {
+      res.status(400);
+      res.send("Name is not valid");
+    }
+  });
+});
+
+//zad.3
+var tab = [];
+app.post("/store", function (req, res) {
+  tab.push(req.body.input);
+  res.status(201);
+  res.json({ stored_data: tab });
+});
+
+console.log("test");
+
+// node ./index.js - [stary] start serwera w terminalu
+// ctrl + c - zatrzymaj serwer
+// nodemon - npm run dev [nowy] start serwera
